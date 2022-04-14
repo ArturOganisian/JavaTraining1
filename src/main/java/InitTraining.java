0e1ab8965ae779cdda929572566b898e788361f6
arturoganisian <artoganis97@gmail.com>
2022-04-14 19:51:39 +0400

    first commit

---
+++ src/main/java/InitTraining.java
@@ -0,0 +1,11 @@
+public class InitTraining {
+
+    public static void main(String[] args) {
+        int num = 12;
+        char a = 'a';
+        boolean isTrue = false;
+        System.out.println(num);
+        System.out.println(a);
+        System.out.println(isTrue);
+    }
+}
---
+++ src/main/java/lesson2/Base.java
@@ -0,0 +1,33 @@
+package lesson2;
+
+import java.sql.SQLOutput;
+
+public class Base {
+    public static void main(String[] args) {
+        boolean isUSA = true;
+        int userAge = 15;
+
+        int minimalAge = isUSA?21:18;
+
+        if(isUSA){
+            minimalAge = 21;
+        }
+        else{
+            minimalAge = 18;
+        }
+
+
+        if (userAge > 100 || userAge < 0){
+            System.out.println("invalid age");
+        }
+        else {
+            if (userAge < 18) {
+                System.out.println("Hmm... your age is under 18.");
+            } else if (userAge == 18) {
+                System.out.println("you can buy only beer");
+            } else {
+                System.out.println("Amazing");
+            }
+        }
+    }
+}
