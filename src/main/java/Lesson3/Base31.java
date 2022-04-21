af64306cf71fb63a1683b52ed69450f3461834bc
arturoganisian <artoganis97@gmail.com>
2022-04-21 14:15:54 +0400

    Homework3 was done. Created the logic to save odd and even numbers in array. Both cases are included with positive and negative numbers. in second part added the logic to find Max Number in entered Array.

--- src/main/java/Lesson3/Base31.java
+++ src/main/java/Lesson3/Base31.java
@@ -1,78 +1,75 @@
 package Lesson3;
 
 public class Base31 {
-        public static void main(String[] args) {
+    public static void main(String[] args) {
 
-            // Task 1: showing off the odd and even numbers for positive and negative cases
+        // Task 1: showing off the odd and even numbers for positive and negative cases
 
-           System.out.println("Task1:");
-           int num = -15;
-           System.out.println("Entered number: " + num);
-           int size = num;
-           if(num<0){
-               size = -size;
-           }
+        System.out.println("Task1:");
+        int num = -15;
+        System.out.println("Entered number: " + num);
+        int size = num;
+        if (num < 0) {
+            size = -size;
+        }
 
-           int[] oddArray = new int[size/2 + 1];
-           int[] evenArray = new int[size/2 + 1];
+        int[] oddArray = new int[size / 2];
+        int[] evenArray = new int[size / 2 + 1];
 
-           int oddArrayIndex = 0;
-           int evenArrayIndex = 0;
+        int oddArrayIndex = 0;
+        int evenArrayIndex = 0;
 
-           if(num<0){
-               for (int i = -num; i > 0; i--) {
-                   if (i % 2 == 0) {
-                       evenArray[evenArrayIndex] = -i;
-                       evenArrayIndex++;
-                   }
-                   else{
-                       oddArray[oddArrayIndex] = -i;
-                       oddArrayIndex++;
-                   }
-               }
-           }
-           else {
-               for (int i = 0; i <= num; i++) {
-                   if (i % 2 == 0) {
-                       evenArray[evenArrayIndex] = i;
-                       evenArrayIndex++;
-                   }
-                   else{
-                       oddArray[oddArrayIndex] = i;
-                       oddArrayIndex++;
-                   }
-               }
-           }
-           System.out.print("Even numbers Array: ");
-           for(int i = 0;i<evenArray.length;i++){
-               System.out.print(evenArray[i] + " ");
-           }
-           System.out.println();
-           System.out.print("Odd numbers Array: ");
-           for(int i = 0;i<oddArray.length;i++){
-               System.out.print(oddArray[i] + " ");
-           }
-           if (num == 0){
-               System.out.print("Array is empty because there is no any odd number");
-           }
-            System.out.println();
-            // Task 2: declaring array and finding max value
-
-            System.out.println();
-            System.out.println("Task2");
-
-            int[] someArray = {-25, -10, - 8,  5, 39, 14, 0, -1, -289, 1000, 103, -7, 1000, -10, 1};
-            System.out.print("Entered the fallowing Array: ");
-            for(int i = 0;i<someArray.length;i++){
-                System.out.print(someArray[i] + " ");
-            }
-            int max = someArray[0];
-            System.out.println();
-            for (int i = 0;i<someArray.length - 1 ;i++) {
-                if (someArray[i] > max) {
-                    max = someArray[i];
+        if (num < 0) {
+            for (int i = -num; i > 0; i--) {
+                if (i % 2 == 0) {
+                    evenArray[evenArrayIndex] = -i;
+                    evenArrayIndex++;
+                } else {
+                    oddArray[oddArrayIndex] = -i;
+                    oddArrayIndex++;
                 }
             }
-            System.out.println("The Max number in this array is: " + max);
+        } else {
+            for (int i = 0; i <= num; i++) {
+                if (i % 2 == 0) {
+                    evenArray[evenArrayIndex] = i;
+                    evenArrayIndex++;
+                } else {
+                    oddArray[oddArrayIndex] = i;
+                    oddArrayIndex++;
+                }
+            }
         }
+        System.out.print("Even numbers Array: ");
+        for (int i = 0; i < evenArray.length; i++) {
+            System.out.print(evenArray[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Odd numbers Array: ");
+        for (int i = 0; i < oddArray.length; i++) {
+            System.out.print(oddArray[i] + " ");
+        }
+        if (num == 0) {
+            System.out.print("Array is empty because there is no any odd number");
+        }
+        System.out.println();
+        // Task 2: declaring array and finding max value
+
+        System.out.println();
+        System.out.println("Task2");
+
+        int[] someArray = {-25, -10, -8, 5, 39, 14, 0, -1, -289, 1000, 103, -7, 1000, -10, 1};
+        System.out.print("Entered the fallowing Array: ");
+        for (int i = 0; i < someArray.length; i++) {
+            System.out.print(someArray[i] + " ");
+        }
+        int max = someArray[0];
+        System.out.println();
+        for (int i = 0; i < someArray.length - 1; i++) {
+            if (someArray[i] > max) {
+                max = someArray[i];
+            }
+        }
+        System.out.println("The Max number in this array is: " + max);
+    }
 }
