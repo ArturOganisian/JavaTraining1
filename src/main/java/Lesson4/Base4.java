ca01e1227b371490891a79034399b0c66bf0b265
arturoganisian <artoganis97@gmail.com>
2022-05-19 14:01:02 +0400

    Banking Homework Redesign.

---
+++ src/main/java/Lesson4/Base4.java
@@ -0,0 +1,14 @@
+package Lesson4;
+
+public class Base4 {
+    public static void main(String[] args) {
+        f();
+    }
+
+    public static void f(){
+        System.out.println("Hello there");
+    }
+    public static void j(){
+        System.out.println("Hello there");
+    }
+}
